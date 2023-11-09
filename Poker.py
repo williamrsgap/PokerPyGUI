a713b6475f49a5b8af1e2fcd702f75ad8f76df3b
williamrsgap <146869255+williamrsgap@users.noreply.github.com>
2023-11-09 13:16:40 -0500

    Updated Logic on File
    File update

--- Poker.py
+++ Poker.py
@@ -3,6 +3,51 @@ def main():
         def __init__(self, value, suit):
             self.value = value
             self.suit = suit
+            
+        def __repr__(self):
+            value_name = ""
+            suit_name = ""
+            match(self.value):
+                case 0:
+                    value_name = "Two"
+                case 1:
+                    value_name = "Three"
+                case 2:
+                    value_name = "Four"
+                case 3:
+                    value_name = "Five"
+                case 4:
+                    value_name = "Six"
+                case 5:
+                    value_name = "Seven"
+                case 6:
+                    value_name = "Eight"
+                case 7:
+                    value_name = "Nine"
+                case 8:
+                    value_name = "Ten"
+                case 9:
+                    value_name = "Jack"
+                case 10:
+                    value_name = "Queen"
+                case 11:
+                    value_name = "King"
+                case 12:
+                    value_name = "Ace"
+                case _:
+                    value_name = ""
+            match(self.suit):
+                case 0:
+                    suit_name = "Spades"
+                case 1:
+                    suit_name = "Diamonds"
+                case 2:
+                    suit_name = "Clubs"
+                case 3:
+                    suit_name = "Hearts"
+                case _:
+                    suit_name = ""
+            return value_name + " " + suit_name
     
     class DeckInital(list):
         def __init__(self):
